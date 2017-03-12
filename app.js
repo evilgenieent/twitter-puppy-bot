cf9f670dc9913a3408f4a9305eecc3e1c363a2ba
fjordski <fordheacock@gmail.com>
2017-03-12 16:51:05 -0500

    Twitter Puppy Bot
    Searches tweets for users who cannot sleep and responds with a picture
    of a puppy.  Has a bank of 50 puppy images to choose from.
    
    Future plans:
    
    >Expand bank of replies
    >Pull images from google instead of local path
    >Build out interactions for replying, favoriting, and retweeting

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const config = require('./config');
+const bodyParser = require('body-parser');
+const Twit = require('twit');
+const T = new Twit(config);
+const request = require('request');
+const exec = require('child_process').exec;
+const fs = require('fs');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+function tweet() {
+    T.get('search/tweets', { q: 'i cant sleep', result_type: 'recent' }, function(err, data) {
+        var userName = data.statuses[0].user.screen_name;
+        // console.log(data.statuses[0]);
+
+        if (data.statuses[0].in_reply_to_user_id !== null ? console.log('it was a retweet') : processing());
+
+        function processing() {
+            let randomNumber = Math.floor(Math.random() * (35)) + 1;
+
+            var filename = 'public/' + randomNumber + '.jpg';
+            var params = {
+                encoding: 'base64'
+            }
+            var b64 = fs.readFileSync(filename, params);
+
+            T.post('media/upload', { media_data: b64 }, uploaded);
+
+            function uploaded(err, data, response) {
+
+                var id = data.media_id_string;
+                var tweet = {
+                    status: '.@' + userName + ' Helo i came to help u slep',
+                    media_ids: [id]
+                }
+
+                T.post('statuses/update', tweet, tweeted);
+
+                function tweeted(err, data, response) {
+                    if (err) {
+                        console.log("Oops!" + err);
+                    } else {
+                        console.log("Yay!");
+                    }
+                }
+
+            }
+
+        }
+    });
+}
+
+tweet();
+var dayInMilliseconds = 1000 * 60 * 60 * 24;
+setInterval(tweet, dayInMilliseconds / 5);
---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by fordheacock on 3/5/17.
+ */
+module.exports = {
+    consumer_key:         'U58s829bNKBw8z00HqcQ5vmD5',
+    consumer_secret:      'gZ3vGKY4SUMb6hpELApG84DjB5he10zZNJaj0wGDzJ3miiwKv5',
+    access_token:         '840938096451584000-bh7ADyVlQIb6Fcc3TUnp5AnS6xsKMWh',
+    access_token_secret:  'PZbPzawAYPQ8RHerFSGPFwSFfJeK2JKl8sja8nklxnF83',
+    timeout_ms:           60*1000
+}
